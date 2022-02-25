fe4aede1b5d3dd746adfbb8ef9739568d0d3fba3
Everton França <everton.r.franca@gmail.com>
2022-02-24 21:59:14 -0300

    Testing the auth route

--- server/routes/API.js
+++ server/routes/API.js
@@ -1,9 +1,16 @@
 import UserController from "../controllers/UserController.js";
 import express from "express";
 import dotenv from "dotenv";
+import Auth from "./auth/common.js";
 
 dotenv.config();
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(Auth.Session);
+app.use(Auth.passport.initialize());
+app.use(Auth.passport.session());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +19,6 @@ app.post("/user", function (req, res) {
 	res.json({ name: req.body.name });
 });
 
-app.get("/user", UserController.getAllUsers);
+app.get("/user", Auth.checkAuthenticated, UserController.getAllUsers);
 
 export default app;
